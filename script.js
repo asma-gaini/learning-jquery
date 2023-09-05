f97481ab48eb50f5baf3b0ec6f7a5bcad6c595cf
asma gaini <asma.gaini@yahoo.com>
2023-09-05 16:27:07 +0430

    jquery learning : slide show in tow button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
     // ba zadan dokme next ax badi namayesh dade mishe
+
     var counter = 0;
     $(".image img").first().siblings("img").hide();   
     $(".next").click(function(e){
@@ -13,7 +14,45 @@ $(document).ready(function(){
        
     });
 
+// ***********************************************************************
 
-    
+    var counter2 = 0;
+
+        hideAll();
+        showSlide(counter2);
+
+    function hideAll(){
+        $(".image2 img").hide();
+    }
+
+    setInterval(function(){nextSlide();} , 5000);
+
+    function showSlide(index){
+        $(".image2 img").eq(index).show();
+    }
+    $(".next2").click(function(){
+        nextSlide();
+    })
+    function nextSlide(){
+       counter2 += 1;
+       if (counter2 >= $(".image2 img").length) {
+            counter2 = 0;
+       }
+       hideAll();
+       showSlide(counter2)
+    }
+
+    $(".prev2").click(function(){
+        prevSlide();
+    })
+    function prevSlide(){
+        counter2 -= 1;
+       if (counter2 < 0) {
+            counter2 = $(".image2 img").length-1;
+       }
+       hideAll();
+       showSlide(counter2)
+    }
+
     // alert("salam");
 })
